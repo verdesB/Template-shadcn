114733d6c0659510052558a8b65e0bc85d709792
verdes ben <verdesb.devacc@gmail.com>
2024-03-01 12:22:36 +0100

    test exluce nodemod

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="react-scripts" />
+declare module 'react-resizable-panel' {
+    export type PanelOnCollapse = (collapsed: boolean) => any;
+    // Autres types personnalisés
+}
